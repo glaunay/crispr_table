262907eaf63b3febaf3a98ac64863ace4d191e2c
sophielem <lematre.sophiegmail.com>
2019-05-13 16:49:05 +0200

    WiP: color cell and bind method in constructor

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -14,24 +14,30 @@ export class MyComponent {
 
   @Prop() str_json: string;
 
+  constructor() {
+    this.onItemClick = this.onItemClick.bind(this);
+  }
   onItemClick(event: Event) {
-    const row = event.currentTarget as HTMLTableRowElement;
+    const cell = event.currentTarget as HTMLTableCellElement;
 
     console.log("Click row")
     console.log(`Current Selected : ${this.rowSelected}`);
 
-    row.style.backgroundColor = (row.style.backgroundColor == 'rgb(204, 204, 204)') ? 'white' : '#ccc';
+    cell.style.backgroundColor = (cell.style.backgroundColor == 'rgb(204, 204, 204)') ? 'white' : '#ccc';
     let tab = document.getElementById("toto") as HTMLTableElement;
     tab.rows[this.rowSelected].style.backgroundColor = 'white';
+    console.log(`ROW : ${cell.parentElement.rowIndex}`)
     // tab.rows[this.rowSelected].cells[this.cellSelected].style.backgroundColor = 'white';
-    this.rowSelected = row.rowIndex;
-    console.log(tab.rows[this.rowSelected].cells)
+    this.rowSelected = cell.parentElement.rowIndex;
+    // console.log(tab.rows[this.rowSelected].cells)
     // this.cellSelected = event.currentTarget.cellIndex;
     // console.log(tab.rows[this.currentSelected])
   }
 
   drawRow(res_json:any): JSX.Element[] {
-    return ([<td >{res_json.sequence}</td>,<td>{res_json.occurences.length}</td>]);
+    console.log("DRAW ROW")
+    console.log(this.rowSelected  );
+    return ([<td onClick={this.onItemClick}>{res_json.sequence}</td>,<td onClick={this.onItemClick}>{res_json.occurences.length}</td>]);
   }
 
   render() {
@@ -43,7 +49,7 @@ export class MyComponent {
       rows.push(this.drawRow(parse_json[i]));
     }
     return ([
-       <table id="toto"> <th> Sequences </th> <th> Occurences </th>{rows.map((d) => <tr onClick={this.onItemClick.bind(this)}> {d} </tr>)} </table>
+       <table id="toto"> <th> Sequences </th> <th> Occurences </th>{rows.map((d) => <tr> {d} </tr>)} </table>
     ]);
   }
 }
