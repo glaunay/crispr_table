5a92092cef9840b4ec7e1b21807f50cec05b8b34
sophielem <sophie.lematre@yahoo.com>
2019-05-14 10:16:21 +0200

    cell white & add an event

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from '@stencil/core';
+import { Component, Prop, Listen, EventEmitter, Event } from '@stencil/core';
 
 
 @Component({
@@ -8,30 +8,34 @@ import { Component, Prop } from '@stencil/core';
 })
 
 export class MyComponent {
-  shadowData:any;
-  private rowSelected:number = 0;
-  // private cellSelected:number = 0;
+  private rowSelected:number = -1;
+  private cellSelected:number = -1;
 
   @Prop() str_json: string;
 
   constructor() {
     this.onItemClick = this.onItemClick.bind(this);
   }
+
+  @Listen('window:scroll')
+  handleScroll(ev) {
+    console.log('the body was scrolled', ev);
+  }
+
+  @Event() jenesaispas: EventEmitter;
+
+  jenesaispasHandler(todo: string) {
+      this.jenesaispas.emit(todo);
+  }
+
   onItemClick(event: Event) {
     const cell = event.currentTarget as HTMLTableCellElement;
 
-    console.log("Click row")
-    console.log(`Current Selected : ${this.rowSelected}`);
-
     cell.style.backgroundColor = (cell.style.backgroundColor == 'rgb(204, 204, 204)') ? 'white' : '#ccc';
     let tab = document.getElementById("toto") as HTMLTableElement;
-    tab.rows[this.rowSelected].style.backgroundColor = 'white';
-    console.log(`ROW : ${cell.parentElement.rowIndex}`)
-    // tab.rows[this.rowSelected].cells[this.cellSelected].style.backgroundColor = 'white';
+    if (this.cellSelected != -1) tab.rows[this.rowSelected].cells[this.cellSelected].style.backgroundColor = 'white';
     this.rowSelected = cell.parentElement.rowIndex;
-    // console.log(tab.rows[this.rowSelected].cells)
-    // this.cellSelected = event.currentTarget.cellIndex;
-    // console.log(tab.rows[this.currentSelected])
+    this.cellSelected = cell.cellIndex;
   }
 
   drawRow(res_json:any): JSX.Element[] {
