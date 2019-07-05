09b8001246be343344241b7b41fab5d8b5394d16
sophielem <lematre.sophiegmail.com>
2019-07-05 12:10:12 +0200

    update stencil

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,7 +1,5 @@
-import { Component, Prop, State, Element} from '@stencil/core';
+import { Component, Prop, State, Element, h } from '@stencil/core';
 import "radial-rep";
-// import fs from 'fs';
-
 
 @Component({
   tag: 'table-crispr',
@@ -173,11 +171,8 @@ fName(seq: string) {
           <a href="#" class="previous round" onClick={() => {if (this.page > 1) this.page -= 1}}>&#8249;</a>
           <a href="#" class="next round" onClick={() => {if (this.page < maxPages) this.page += 1}}>&#8250;</a>
         </div>
-      </div>,
+      </div>
 
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>,
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>,
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
       ]);
   }
 }
