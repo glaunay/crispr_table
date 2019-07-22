2646d6744180b51082f614fe902e178553b8ef9f
sophielem <lematre.sophiegmail.com>
2019-07-22 15:06:57 +0200

    spinner

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -151,6 +151,10 @@ getDicSeq(seq: string):string {
 
     }
     return ([
+      <head>
+        //@ts-ignore
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+      </head>,
       // ***********************************************
       // ******************* SPINNER *******************
       // ***********************************************
